68c9bd137b622b3e1132dc7cf84e8502f3722f1d
blahkheart <dannithomx@gmail.com>
2020-09-24 22:58:53 +0100

    custom back-button designed

--- extras.js
+++ extras.js
@@ -1,4 +1,3 @@
-
 function switchFormsSection (trigger, formId, spawn) {
     trigger.on('click', function () {
         trigger.toggleClass('hidden');
@@ -7,3 +6,25 @@ function switchFormsSection (trigger, formId, spawn) {
     })
 }
   
+ // User category selection confirmation
+ $('.psu-radio').change(function () {
+    swal({
+      title: "Confirmation!",
+      text: "Are you sure you want to proceed as a "+ $(this).val()+ '?',
+      icon: "warning",
+      buttons: ['No', 'Yes'],
+    }).then((value) => {
+        if (value) {
+          swal("you selected " + $(this).val(), {
+            icon: "success",
+          });
+        } else {
+          swal({
+            text:"Please select a category to continue"});
+          // swal("Please select a category to continue");
+          // unselects the selected option by choosing a psuedo option
+          $('.psuedo-option').trigger('click');
+        }
+      });
+
+   })
