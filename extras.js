c0a7be4ccfd5dcde19e565cd64afa4c55d8d859d
blahkheart <dannithomx@gmail.com>
2020-09-29 16:27:29 +0100

    added animation between signup form 1st and 2nd steps, updated validation rules and messages, cleaned up comments

--- extras.js
+++ extras.js
@@ -6,70 +6,93 @@ function switchFormsSection (trigger, formId, spawn) {
     })
 }
   
- // User category selection confirmation
- $('.psu-radio').change(function () {
-    swal({
-      title: "Confirmation!",
-      text: "Are you sure you want to proceed as a "+ $(this).val()+ '?',
-      icon: "warning",
-      buttons: ['No', 'Yes'],
-    }).then((value) => {
-        if (value) {
-          swal("you selected " + $(this).val(), {
-            icon: "success",
-          });
-        } else {
-          swal({
-            text:"Please select a category to continue"});
-          $('.psuedo-option').trigger('click');
-        }
-      });
-    })
+// User category selection confirmation
+$('.psu-radio').change(function () {
+swal({
+title: "Confirmation!",
+text: "Are you sure you want to proceed as a "+ $(this).val()+ '?',
+icon: "warning",
+buttons: ['No', 'Yes'],
+}).then((value) => {
+if (value) {
+swal("you selected " + $(this).val(), {
+  icon: "success",
+});
+} else {
+swal({
+  text:"Please select a category to continue"});
+$('.psuedo-option').trigger('click');
+}
+});
+})
 
-    // CONFIRM TRANSACTION 
-   function confirmTransaction (e) {
-    var checker = e.preventDefault();
-    $(this).bind(checker);
-       swal({
-           title:"Confirmation!",
-           text: "Are you sure you want to proceed with this transaction",
-           buttons: true,
-           dangerMode: true,
-       })
-       .then((value) => {
-         if(value === true) {
-           $(this).unbind(checker);
-           $(this).submit();
-           swal({
-               title: "Information!",
-               text: "Your request has been submitted and is being processed",
-               icon: "success",
-           })
-           // $(this).unbind(checker);
-         } else {
-           swal("Aborted!!", "Your request was not submitted");
-         }
-       }
-     )}
+// CONFIRM TRANSACTION 
+function confirmTransaction (e) {
+var checker = e.preventDefault();
+$(this).bind(checker);
+swal({
+  title:"Confirmation!",
+  text: "Are you sure you want to proceed with this transaction",
+  buttons: true,
+  dangerMode: true,
+})
+.then((value) => {
+if(value === true) {
+  $(this).unbind(checker);
+  $(this).submit();
+  swal({
+      title: "Information!",
+      text: "Your request has been submitted and is being processed",
+      icon: "success",
+  })
+  // $(this).unbind(checker);
+} else {
+  swal("Aborted!!", "Your request was not submitted");
+}
+}
+)}
 
-     // dashboard Farmlist tabs ruler switching
-     function sectionTabSwitch (trigger, arr) {
-         $(trigger).click(function(){
-             $(arr).each(function(index, element){
-                $(element).next().removeClass('hidden');
-             });
-     
-             $(this).next().addClass('hidden');
-         });
-     }
-        
-    // $(function () {
-    //   $('#psu-signup').parsley().on('field:validated', function() {
-    //     var ok = $('.parsley-error').length === 0;
-    //     $('.bs-callout-info').toggleClass('hidden', !ok);
-    //     $('.bs-callout-warning').toggleClass('hidden', ok);
-    //   })
-    //   .on('form:submit', function() {
-    //     return true;
-    //   });
-    // });
+// dashboard Farmlist tabs ruler switching
+function sectionTabSwitch (trigger, arr) {
+$(trigger).click(function(){
+    $(arr).each(function(index, element){
+      $(element).next().removeClass('hidden');
+    });
+
+    $(this).next().addClass('hidden');
+});
+}
+
+$(function () {
+  $('#psu-signup').parsley().on('field:validated', function() {
+    var ok = $('.parsley-error').length === 0;
+    $('.bs-callout-info').toggleClass('hidden', !ok);
+    $('.bs-callout-warning').toggleClass('hidden', ok);
+  })
+  .on('form:submit', function() {
+    return true;
+  });
+});
+
+function sectionTabSwitch (trigger, arr) {
+  $(trigger).click(function(){
+    $(arr).each(function(index, element){
+      $(element).next().removeClass('hidden');
+    });
+    $(this).next().addClass('hidden');
+    function hideMe () {
+      $('#user-type').addClass('hidden');
+    }
+    function showOther () {
+      $('#user-contact').removeClass('hidden');
+    }
+    hideMe();
+    showOther();
+  });
+}
+
+//Toggle remove-content class to 1st jumbotron column 
+$('#psu-signup-btn').click(function ()  { $('#first-column').toggleClass('remove-content')})
+
+//Remove col-md-6 class from 2nd jumbotron column to make it full width
+$('#student-btn').click(function ()  { $('#second-column').removeClass('col-md-6')})
